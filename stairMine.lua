405d2f73e607ed5439c2d82d9ea6193a02285ef5
Eric Jayjock <ejayjock@gmail.com>
2020-07-17 13:31:24 -0400

    Uncle E made some readability edits.

--- stairMine.lua
+++ stairMine.lua
@@ -11,37 +11,38 @@ Series of turtle commands:
 
 
 function clearUp()
-    cont=true
-    while cont do
-      if turtle.detectUp() then
-        turtle.digUp()
-      else
-        cont=false
-      end
+  cont=true
+  while cont do
+    if turtle.detectUp() then
+      turtle.digUp()
+    else
+      cont=false
     end
   end
+end
 
-    turtle.up()
-    --turtle.digUp()
-    cont=true
-    while cont do
-      if turtle.detectUp() then
-        turtle.digUp()
-      else
-        cont=false
-      end
-    end
-    turtle.down()
-    turtle.digDown()
-    turtle.down()
-    --turtle.dig()
-    cont=true
-    while cont do
-      if turtle.detect() then
-        turtle.dig()
-      else
-        cont=false
-      end
-    end
-    turtle.forward()
+turtle.up()
+--turtle.digUp()
+cont=true
+while cont do
+  if turtle.detectUp() then
+    turtle.digUp()
+  else
+    cont=false
+  end
+end
+turtle.down()
+turtle.digDown()
+turtle.down()
+--turtle.dig()
+cont=true
+while cont do
+  if turtle.detect() then
+    turtle.dig()
+  else
+    cont=false
+  end
+end
+turtle.forward()
+
 end
